b2793b594bba26aaa28df6a93b66c9b06cb9d604
casellas <b.casellas12@gmail.com>
2024-07-14 01:46:55 -0400

    actual

--- pagina_ing/core/models.py
+++ pagina_ing/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Documentos(models.Model):
+    codigo = models.CharField(max_length=4, primary_key=True)
+    nombre_caso = models.CharField(max_length=250)
+    archivo = models.FileField(upload_to='documentos/')
